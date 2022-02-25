5d319380281c76f3ad940d58527d712f212305e5
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-02-25 20:26:13 +0600

    react core concepts first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = {
+    name : "Firoz",
+    lname : "Alam"
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {name.name + " "+name.lname}</h1>
+       
       </header>
     </div>
   );
